3db52f5039726b9b7cd7cedc7c0dcd529c580f9c
Alex Albu <alexp.albu@gmail.com>
2016-10-23 15:19:26 +0200

    Master Profile is secured fixes #45
    Master Profile is secured fixes #45

--- src/app/pages/ProfilePage/services/app-profile.service.ts
+++ src/app/pages/ProfilePage/services/app-profile.service.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../../services/auth.service';
 import { Injectable }  from '@angular/core';
 import { Http,Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -7,16 +8,16 @@ import {BaseService} from "../../../services/base.service";
 
 @Injectable()
 export class AppProfileService extends BaseService{
-    constructor (private http: Http) {
+    constructor (private http: Http, private auth: AuthService) {
         super(http);
     }
 
     getAppProfile():Observable<AppProfile> {
-
-        return this.http.get(this.rootAddress + 'masterprofile')
-            .map(this.extractData)
-            .catch(this.handleError);
-
+        return this.auth.loginSentinel().flatMap(() => {
+            return this.http.get(this.rootAddress + 'masterprofile', {headers: this.auth.headers})
+                .map(this.extractData)
+                .catch(this.handleError);
+        });
 
     }
 
