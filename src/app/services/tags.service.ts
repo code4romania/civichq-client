a4275f8c8f4469cf6f44cc44a0972b470dabc734
Alex Albu <alexp.albu@gmail.com>
2016-10-23 21:14:29 +0200

    Secured search and tags endpoints fixes #44 #43
    Secured search and tags endpoints fixes #44 #43

--- src/app/services/tags.service.ts
+++ src/app/services/tags.service.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { Response, Http,Headers } from '@angular/http';
 import { Observable } from 'rxjs';
@@ -5,15 +6,18 @@ import { BaseService } from "./base.service";
 
 @Injectable()
 export  class TagsService extends BaseService {
-    constructor(private http:Http) {
+    constructor(private http:Http, private auth: AuthService) {
         super(http);
     }
 
     getTags(searchTerm){
         const url = `${this.rootAddress + 'tags'}/${searchTerm}`;
-        return this.http.get(url)
-                    .map((response:Response)=>{
-                        return response.json();
-                    })
+        return this.auth.loginSentinel().flatMap(() => {
+            return this.http.get(url, {headers: this.auth.headers})
+                .map((response: Response) => {
+                    return response.json();
+                })
+        })
+
     }
 }
